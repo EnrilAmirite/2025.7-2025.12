f1efd68e79ae6bd72996401873e5b963faab2042
Enril <105336446+EnrilAmirite@users.noreply.github.com>
2025-07-04 10:38:15 +0800

    Delete 2025.6-2025.8练习/transformer的实现.py

--- 2025.6-2025.8练习/transformer的实现.py
+++
@@ -1,9 +0,0 @@
-# 这里用transformer架构实现一个情感分类模型。。。
-# 数据库是经典的IMDB
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import math
-from collections import Counter
-from datasets import load_dataset
-from torch.utils.data import Dataset, DataLoader
